c077a0169c1ec22069b89d841bb3233d5fd9f314
bharatmk257 <bharatmk257@gmail.com>
2019-01-11 10:27:21 +0530

    fade animation

--- app/src/main/java/com/bharatmk257/androidanimations/MainActivity.java
+++ app/src/main/java/com/bharatmk257/androidanimations/MainActivity.java
@@ -2,12 +2,36 @@ package com.bharatmk257.androidanimations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHelloWorld;
+    private TextView txtHiWorld;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtHelloWorld = (TextView)findViewById(R.id.helloWorld);
+        txtHiWorld = (TextView)findViewById(R.id.hiWorld);
+
+        txtHelloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this,"Hello World",Toast.LENGTH_LONG).show();
+
+//                txtHelloWorld.animate().scaleY(90f).setDuration(1000);
+//                txtHelloWorld.animate().scaleX(90f).setDuration(1000);
+                txtHelloWorld.animate().alpha(0.0f).setDuration(1000);
+                txtHiWorld.animate().alpha(1.0f).setDuration(1000);
+
+            }
+        });
+
     }
 }
