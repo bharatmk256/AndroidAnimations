6784873b44db13849b7c6efa7f5266aebcd01c98
bharatmk257 <bharatmk257@gmail.com>
2019-01-11 11:44:02 +0530

    animation on image

--- app/src/main/java/com/bharatmk257/androidanimations/MainActivity.java
+++ app/src/main/java/com/bharatmk257/androidanimations/MainActivity.java
@@ -3,6 +3,7 @@ package com.bharatmk257.androidanimations;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,15 +11,22 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView txtHelloWorld;
     private TextView txtHiWorld;
+    private ImageView imgWolf;
+    private ImageView imgComedy;
 
+    boolean isHelloWorldShowing;
+    boolean isWolfShowing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtHelloWorld = (TextView)findViewById(R.id.helloWorld);
-        txtHiWorld = (TextView)findViewById(R.id.hiWorld);
+        txtHelloWorld = (TextView) findViewById(R.id.helloWorld);
+        txtHiWorld = (TextView) findViewById(R.id.hiWorld);
+        imgComedy = (ImageView) findViewById(R.id.comedy);
+        imgWolf = (ImageView) findViewById(R.id.wolf);
+
 
         txtHelloWorld.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,9 +35,34 @@ public class MainActivity extends AppCompatActivity {
 
 //                txtHelloWorld.animate().scaleY(90f).setDuration(1000);
 //                txtHelloWorld.animate().scaleX(90f).setDuration(1000);
-                txtHelloWorld.animate().alpha(0.0f).setDuration(1000);
-                txtHiWorld.animate().alpha(1.0f).setDuration(1000);
+//                txtHelloWorld.animate().alpha(0.0f).setDuration(3000);
+//                txtHiWorld.animate().alpha(1.0f).setDuration(3000);
 
+                if (isHelloWorldShowing) {
+                    txtHelloWorld.animate().alpha(0.0f).setDuration(1000);
+                    txtHiWorld.animate().alpha(1.0f).setDuration(1000);
+                    isHelloWorldShowing = false;
+                }else if(!isHelloWorldShowing){
+                    txtHelloWorld.animate().alpha(1.0f).setDuration(1000);
+                    txtHiWorld.animate().alpha(0.0f).setDuration(1000);
+                    isHelloWorldShowing = true;
+                }
+            }
+        });
+
+
+        imgWolf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isWolfShowing){
+                    imgWolf.animate().alpha(0).setDuration(1000);
+                    imgComedy.animate().alpha(1).setDuration(1000);
+                    isWolfShowing = false;
+                }else if (!isWolfShowing){
+                    imgWolf.animate().alpha(1).setDuration(1000);
+                    imgComedy.animate().alpha(0).setDuration(1000);
+                    isWolfShowing = true;
+                }
             }
         });
 
